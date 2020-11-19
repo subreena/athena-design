bbd32bf3cae315dbf1e4411f6916a4c335fb53b1
Shoyas <md.nasiruddin4067@gmail.com>
2020-11-20 02:58:04 +0600

    task is completed

--- src/Components/Home/Contact/Contact.js
+++ src/Components/Home/Contact/Contact.js
@@ -1,11 +1,80 @@
 import React from 'react';
+import './Contact.css';
+import logo from '../../images/logo.png';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBehance, faFacebook, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faBasketballBall} from '@fortawesome/free-solid-svg-icons';
+
+
+
 
 const Contact = () => {
     return (
     <>
-    <section>
-    <h2 className="section-title">Get your design right, right now</h2>
-    </section>
+        <section className="contact-container text-center">
+
+            <div className="contact-header mb-5 mt-5">
+                <h2 className="section-title">Get your design right, right now</h2>
+                <p style={{color:'gray'}}>Be the first know our latest offers and updates!</p>
+            </div>
+            <div className="mt-5">
+                <label htmlFor="">
+                    <input type="text" name="" id="" placeholder="Enter your email address"/>
+                    <button className="contact-btn">Get Started</button>
+                </label>
+            </div>
+            <div className="container-fluid footer-body">
+                <div className="row">
+                    <div className="col-md-4">
+                        <img src={logo} alt=""/>
+
+                        <div className="icon-part">
+                            <FontAwesomeIcon className="icon-item" icon={faFacebook} />
+                            <FontAwesomeIcon className="icon-item" icon={faTwitter} />
+                            <FontAwesomeIcon className="icon-item" icon={faLinkedinIn} />
+                            <FontAwesomeIcon className="icon-item" icon={faBasketballBall} />
+                            <FontAwesomeIcon className="icon-item" icon={faBehance} />
+                        </div>
+
+                    </div>
+                    <div className="col-md-8">
+                        <div className="row">
+                            <div className="col-md-4">
+                                <ul className="footer-item">
+                                    <li><Link className="nav-link" to="#">Features</Link></li>
+                                    <li><Link className="nav-link" to="#">Enterprise</Link></li>
+                                    <li><Link className="nav-link" to="#">Pricing</Link></li>
+                                </ul>
+
+                            </div>
+                            <div className="col-md-4">
+                                <ul className="footer-item">
+                                    <li><Link className="nav-link" to="#">Blog</Link></li>
+                                    <li><Link className="nav-link" to="#">Help Center</Link></li>
+                                    <li><Link className="nav-link" to="#">Contact Us</Link></li>
+                                    <li><Link className="nav-link" to="#">Status</Link></li>
+                                </ul>
+
+                            </div>
+                            <div className="col-md-4">
+                                <ul className="footer-item">
+                                    <li><Link className="nav-link" to="#">About Us</Link></li>
+                                    <li><Link className="nav-link" to="#">Terms of Service</Link></li>
+                                    <li><Link className="nav-link" to="#">Security</Link></li>
+                                    <li><Link className="nav-link" to="#">Login</Link></li>
+                                </ul>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            
+
+        </section>
     </>
     );
 };
--- src/Components/Home/Pricing/Pricing.js
+++ src/Components/Home/Pricing/Pricing.js
@@ -1,11 +1,87 @@
 import React from 'react';
+import './Pricing.css';
 
 const Pricing = () => {
     return (
        <>
-       <section>
-      <h2 className="section-title">Choose Your Dedicated Team</h2>
-       </section>
+        <section className="pricing-container container-fluid">
+            <h2 className="section-title mb-5 text-center">Choose Your Dedicated Team</h2>
+            <div className="row">
+                <div className="col-md-4 p-5">
+                    <div class="card p-3">
+                        <div class="card-body">
+                            <div className="text-center">
+                                <h1 class="card-title">$199</h1>
+                                <h6 className="price-tag">For Basic</h6>
+                            </div>
+                            <p class="card-text text-center global-list p-3">
+                                <ul className="pr-4">
+                                    <li><h6>Homepage</h6></li>
+                                    <li><h6>No Inner Page</h6></li>
+                                    <li><h6>Asset file</h6></li>
+                                    <li><h6>Source file</h6></li>
+                                    <li><h6>Free Stock Photos</h6></li>
+                                    <li><h6>10 Days Free Support</h6></li>
+                                    <li><h6>24/7 Support</h6></li>
+                                </ul>
+                            </p>
+                            <div className="pricing-btn">
+                                <button id="" className="global-btn">Order Now</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-4 p-5">
+                    <div class="card p-3">
+                        <div class="card-body">
+                                <div className="text-center">
+                                    <h1 class="card-title">$399</h1>
+                                    <h6 className="price-tag">For Preferred</h6>
+                                </div>
+                                <p class="card-text text-center global-list p-3">
+                                    <ul className="pr-4">
+                                        <li><h6>Homepage</h6></li>
+                                        <li><h6>4 Inner Pages</h6></li>
+                                        <li><h6>Asset file</h6></li>
+                                        <li><h6>Source file</h6></li>
+                                        <li><h6>Free Stock Photos</h6></li>
+                                        <li><h6>20 Days Free Support</h6></li>
+                                        <li><h6>24/7 Support</h6></li>
+                                    </ul>
+                                </p>
+                                <div className="pricing-btn">
+                                    <button id="" className="global-btn">Order Now</button>
+                                </div>
+                            </div>                                           
+                        </div>
+                </div>
+                <div className="col-md-4 p-5">
+                    <div class="card p-3">
+                        <div class="card-body">
+                            <div className="text-center">
+                                <h1 class="card-title">$599</h1>
+                                <h6 className="price-tag">For Elite</h6>
+                            </div>
+                            <p class="card-text text-center global-list p-3">
+                                <ul className="pr-4">
+                                    <li><h6>Homepage</h6></li>
+                                    <li><h6>8 Inner Pages</h6></li>
+                                    <li><h6>Asset file</h6></li>
+                                    <li><h6>Source file</h6></li>
+                                    <li><h6>Free Stock Photos</h6></li>
+                                    <li><h6>30 Days Free Support</h6></li>
+                                    <li><h6>24/7 Support</h6></li>
+                                </ul>
+                            </p>
+                            <div className="pricing-btn">
+                                <button id="" className="global-btn">Order Now</button>
+                            </div>
+                        </div>                                
+                    </div>
+                </div>
+            </div>    
+
+        </section>
        </>
     );
 };
